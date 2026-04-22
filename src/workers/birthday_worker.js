60aadf0b51162f1145b0e510c7e9f9902d15cc56
arieesetiawan <arieesetiawann@gmail.com>
2026-04-22 17:12:38 +0700

    fix date inconsistency on worker

--- src/workers/birthday_worker.js
+++ src/workers/birthday_worker.js
@@ -4,6 +4,7 @@ const { calculateNextBirthday } = require('../lib/birthday_calculator');
 const config = require('../config/env.js');
 const emailLib = require("../lib/email");
 const { HttpError } = require('../lib/error');
+const { DateTime } = require("luxon");
 
 agenda.define(
   'send birthday',
@@ -15,16 +16,20 @@ agenda.define(
       user = await Users.findById(userId);
       if (!user) return;
 
-      const now = new Date();
-      const currentYear = now.getFullYear()
+      const now = DateTime.now().setZone(user.timezone);
+      const currentYear = now.year
+
       let next = calculateNextBirthday(user.birthday, user.timezone);
       if (!next || isNaN(new Date(next))) {
         console.error("Invalid next birthday", userId);
         return;
       }
 
-      if (next <= now) {
-        next.setFullYear(next.getFullYear() + 1);
+      let nextDt = DateTime.fromJSDate(next).setZone(user.timezone);
+
+      if (nextDt <= now) {
+        nextDt = nextDt.plus({ years: 1 });
+        next = nextDt.toUTC().toJSDate();
       }
 
       if (user.lastBirthdaySentYear === currentYear) {
@@ -33,10 +38,19 @@ agenda.define(
         return;
       }
 
-      const birthday = user.birthday
-      const isBirthday =
-        birthday.getUTCDate() === now.getUTCDate() &&
-        birthday.getUTCMonth() === now.getUTCMonth();
+      const birthday = DateTime.fromJSDate(user.birthday).setZone(user.timezone);
+      const startOfBirthday = now.set({
+        month: birthday.month,
+        day: birthday.day,
+        hour: 0,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+      });
+
+      const endOfBirthday = startOfBirthday.plus({ days: 1 });
+
+      const isBirthday = now >= startOfBirthday && now < endOfBirthday;
 
       if (isBirthday) {
         const updated = await Users.findOneAndUpdate(
